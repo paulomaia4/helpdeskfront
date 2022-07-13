689539199ed5654e5a168f150be7dfbba47565ab
paulomaia4 <paulomaia4@hotmail.com>
2022-07-13 09:35:12 -0300

    componente header implantado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MatCardModule } from '@angular/material/card';
 // Componentes do projeto 
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
@@ -40,7 +41,8 @@ import { HomeComponent } from './components/home/home.component';
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent
   ],
 
   imports: [
